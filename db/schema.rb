ff254a53a8abd4829202601cd8efcd8b0f13dccd
Katherine Szelag <katherine.szelag@mail.utoronto.ca>
2020-02-14 16:32:47 -0500

    user interest table made and seeded

--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 2020_02_14_172601) do
+ActiveRecord::Schema.define(version: 2020_02_14_195155) do
 
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
@@ -20,6 +20,12 @@ ActiveRecord::Schema.define(version: 2020_02_14_172601) do
     t.text "question"
     t.datetime "created_at", precision: 6, null: false
     t.datetime "updated_at", precision: 6, null: false
+  end
+
+  create_table "user_interests", force: :cascade do |t|
+    t.integer "user_id"
+    t.integer "interest_id"
+    t.string "value"
   end
 
   create_table "users", force: :cascade do |t|
--- db/seeds.rb
+++ db/seeds.rb
@@ -12,3 +12,5 @@ Interest.create(name: "pet_name", question: "Do you have any pets?")
 Interest.create(name: "friend_name", question: "Mind giving me the first name of one of your close friends.")
 Interest.create(name: "hobby", question: "Fill in the blank with a hobby of yours: I like to ")
 Interest.create(name: "sport", question: "Do you play any or watch any sports? (If yes, type the name of one here)")
+
+UserInterest.create(user_id: 1, interest_id: 1, value: "Charlie")
