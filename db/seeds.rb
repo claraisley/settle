6bf7b3bad53d9dad9dc98e19f716ed04aa457844
Katherine Szelag <katherine.szelag@mail.utoronto.ca>
2020-02-14 14:32:46 -0500

    interests table seeded

--- db/seeds.rb
+++ db/seeds.rb
@@ -8,4 +8,7 @@
 
 User.create(first_name: "Katherine", last_name: "Szelag", email: "katherine.szelag@alum.utoronto.ca")
 
-Interest
+Interest.create(name: "pet_name", question: "Do you have any pets?")
+Interest.create(name: "friend_name", question: "Mind giving me the first name of one of your close friends.")
+Interest.create(name: "hobby", question: "Fill in the blank with a hobby of yours: I like to ")
+Interest.create(name: "sport", question: "Do you play any or watch any sports? (If yes, type the name of one here)")
