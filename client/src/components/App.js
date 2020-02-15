be188327fbf9f83abcd7cf5e1bafe3486b68ce67
Susan Zhang <susanwenzhang@gmail.com>
2020-02-15 16:42:19 -0500

    made axios calls to thinkingtraps and tipstricks and loaded data into app state

--- client/src/components/App.js
+++ client/src/components/App.js
@@ -12,9 +12,10 @@ import useApplicationData from "../hooks/useApplicationData.js";
 function App() {
 
   const {
-    pages, links
+    state, links
   } = useApplicationData();
 
+  console.log("state", state)
   // getPages
   // getUsers
 
--- client/src/hooks/useApplicationData.js
+++ client/src/hooks/useApplicationData.js
@@ -10,11 +10,19 @@ import Login from '../components/Login.js';
 export default function useApplicationData() {
 
   //pages state
-  const [pages, setPages] = useState([])
+  const [state, setState] = useState({
+    pages: {
+      thinkingTraps: [],
+      tipsTricks: [],
+      testAnxiety: [],
+      meditations: []
+    }
+  })
 
   useEffect(() => {
+    Promise.all([
     axios.request({
-      url: 'http://localhost:3001/users',
+      url: 'http://localhost:3001/thinking_traps',
       method: 'get',
       headers: {
         'Content-Type': 'application/json',
@@ -22,8 +30,25 @@ export default function useApplicationData() {
         'Access-Control-Allow-Credentials': true
       },
       withCredentials: true
-    }).then(function (response) {
-      console.log(response);
+    }), axios.request({
+      url: 'http://localhost:3001/tips',
+      method: 'get',
+      headers: {
+        'Content-Type': 'application/json',
+        'Accept': 'application/json',
+        'Access-Control-Allow-Credentials': true
+      },
+      withCredentials: true
+    })])
+    .then((response) => {
+      setState(prev => ({
+        ...prev,
+        pages: {
+          ...prev.pages,
+          thinkingTraps: response[0].data,
+          tipsTricks: response[1].data
+        }
+      }))
     })
       .catch(function (error) {
         console.log(error);
@@ -32,34 +57,52 @@ export default function useApplicationData() {
 
 
   const links = [
-    { name: "Signup", 
+    {
+      name: "Signup",
       path: "/signup",
-      component: <Signup /> },
-    { name: "Login", 
-      path: "/login", 
-     component: <Login /> },
-    { name: "Menu", 
-      path: "/menu", 
-     component: <Menu />  },
-    { name: "Workthrough", 
-      path: "/workthrough", 
-     component: <Workthrough />  },
-    { name: "Progress", 
-      path: "/progress", 
-     component: <Progress />  },
-    { name: "What is test anxiety?",
+      component: <Signup />
+    },
+    {
+      name: "Login",
+      path: "/login",
+      component: <Login />
+    },
+    {
+      name: "Menu",
+      path: "/menu",
+      component: <Menu />
+    },
+    {
+      name: "Workthrough",
+      path: "/workthrough",
+      component: <Workthrough />
+    },
+    {
+      name: "Progress",
+      path: "/progress",
+      component: <Progress />
+    },
+    {
+      name: "What is test anxiety?",
       path: "/test-anxiety",
-      component: <StaticPage data={pages.testAnxiety}  />  },
-    { name: "Thinking Traps",
+      component: <StaticPage data={state.pages.testAnxiety} />
+    },
+    {
+      name: "Thinking Traps",
       path: "/thinking-traps",
-      component: <StaticPage data={pages.thinkingTraps} /> },
-    { name: "Tips/Tricks",
+      component: <StaticPage data={state.pages.thinkingTraps} />
+    },
+    {
+      name: "Tips/Tricks",
       path: "/tips-tricks",
-      component: <StaticPage data={pages.tipsTricks} /> },
-    { name: "Meditations",
+      component: <StaticPage data={state.pages.tipsTricks} />
+    },
+    {
+      name: "Meditations",
       path: "/meditations",
-      component: <StaticPage data={pages.meditations} /> }
+      component: <StaticPage data={state.pages.meditations} />
+    }
   ]
 
-  return { pages, links }
+  return { state, links }
 };
