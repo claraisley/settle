84009a78140fc6e96f3c4b8f6886bea07c36ce9e
Susan Zhang <susanwenzhang@gmail.com>
2020-02-15 16:57:44 -0500

    removed dummy data

--- client/src/components/StaticPage.js
+++ client/src/components/StaticPage.js
@@ -3,14 +3,6 @@ import StaticItem from "./StaticItem.js";
 
 export default function StaticPage(props) {
 
-  console.log("static page data", props.data)
-
-  const meditations = [
-    {"id": 1, "name": "One minute", "value": "www.google.ca"},
-    {"id": 2, "name": "Two minutes", "value": "www.google.ca"},
-    {"id": 3, "name": "Three minutes", "value": "www.google.ca"}
-  ]
-
   const items = props.data.map(data => {
     return <StaticItem
     key={data.id}
