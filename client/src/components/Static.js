871a99316ecfd50e47e102e48b4203e277c5240f
Susan Zhang <susanwenzhang@gmail.com>
2020-02-15 12:48:33 -0500

    rendered static items with mocked data

--- client/src/components/Static.js
+++ client/src/components/Static.js
@@ -1,11 +1,27 @@
 import React from "react";
 import StaticItem from "./StaticItem.js";
 
-export default function Static() {
+export default function Static(props) {
+
+  const meditations = [
+    {"id": 1, "name": "One minute", "value": "www.google.ca"},
+    {"id": 2, "name": "Two minutes", "value": "www.google.ca"},
+    {"id": 3, "name": "Three minutes", "value": "www.google.ca"}
+  ]
+
+  const items = meditations.map(meditation => {
+    return <StaticItem
+    key={meditation.id}
+    name={meditation.name}
+    value={meditation.value} />
+  })
+
+  console.log(items)
+
   return (
     <main className="static">
       <h2>Static</h2>
-      <StaticItem />
+      {items}
     </main>
   )
 }
--- client/src/components/StaticItem.js
+++ client/src/components/StaticItem.js
@@ -1,9 +1,10 @@
 import React from "react";
 
-export default function StaticItem() {
+export default function StaticItem(props) {
   return (
     <div className="staticItem">
-      <h3>Static Item</h3>
+      <h3>{props.name}</h3>
+      <p>{props.value}</p>
     </div>
   )
 }
