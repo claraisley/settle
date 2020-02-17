b39213f149fa8ba83f9da657e5d858cc4ba15315
Susan Zhang <susanwenzhang@gmail.com>
2020-02-17 11:19:02 -0500

    added completion and followup modes

--- client/src/components/Workthrough/Index.js
+++ client/src/components/Workthrough/Index.js
@@ -2,9 +2,13 @@ import React from "react";
 import Mood from "./Mood.js";
 import Question from "./Question.js";
 import useVisualMode from "../../hooks/useVisualMode"
+import Followup from "./Followup.js";
+import Completion from "./Completion.js";
 
 const MOOD = "MOOD";
 const QUESTION = "QUESTION";
+const FOLLOWUP = "FOLLOWUP";
+const COMPLETION = "COMPLETION";
 
 export default function Workthrough() {
 
@@ -16,6 +20,12 @@ export default function Workthrough() {
       <section>
         {mode === MOOD && <Mood />}
         {mode === QUESTION && <Question />}
+        {mode === FOLLOWUP && <Followup />}
+        {mode === COMPLETION && <Completion />}
+      </section>
+      <section>
+        <label htmlFor="workthrough-progress">Progress</label>
+        <progress id="workthrough-progress"></progress>
       </section>
     </main>
   )
