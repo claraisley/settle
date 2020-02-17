56ab8060955c52294faf4b4dfe904b69aa30ebb1
Susan Zhang <susanwenzhang@gmail.com>
2020-02-17 16:38:04 -0500

    at least i'm setting state properly now

--- client/src/components/Workthrough/Index.js
+++ client/src/components/Workthrough/Index.js
@@ -20,6 +20,8 @@ export default function Workthrough() {
     responses: []
   })
 
+  console.log("STATE", state)
+
   const { mode, transition, back } = useVisualMode(START);
 
   const startWorkthrough = (numberOfQuestions) => {
@@ -33,6 +35,7 @@ export default function Workthrough() {
   }
 
   const currentQuestion = state.questions.find(question => {
+    console.log("currentQuestion")
     return question.answered === false;
   })
 
@@ -40,7 +43,24 @@ export default function Workthrough() {
     return response.thought === currentQuestion.id;
   })
 
+  const currentProgress = state.questions.filter(question => {
+    return question.answered === true
+  }).length
+
+  const respond = () => {
+    console.log("respond")
+    setState(prev => ({
+      ...prev,
+      questions: [
+        ...state.questions.filter(({ id }) => id !== currentQuestion.id),
+        { ...currentQuestion, answered: true }
+      ],
+    }))
+    startNextQuestion();
+  }
+
   const startNextQuestion = () => {
+    console.log("start next question")
     transition(QUESTION)
   }
 
@@ -51,13 +71,14 @@ export default function Workthrough() {
         {mode === START && <Start
           startWorkthrough={startWorkthrough} />}
         {mode === MOOD && <Mood />}
-        {mode === QUESTION && <Question question={currentQuestion} responses={currentResponses} />}
+        {mode === QUESTION && <Question question={currentQuestion} responses={currentResponses} onResponse={respond} />}
         {mode === FOLLOWUP && <Followup />}
         {mode === COMPLETION && <Completion />}
       </section>
       <section>
-        <label htmlFor="workthrough-progress">Progress</label>
-        <progress id="workthrough-progress"></progress>
+        <label htmlFor="workthrough-progress">Progress {currentProgress}/{state.questions.length}</label>
+        <progress id="workthrough-progress" value="{currentProgress}/{state.questions.length}" max="100" ></progress>
+        {/* <LinearProgress variant="determinate" value={completed} /> */}
         <span role="img" aria-label="up-arrow">⬆</span>
         <span role="img" aria-label="down-arrow">⬇</span>
       </section>
--- client/src/components/Workthrough/Question.js
+++ client/src/components/Workthrough/Question.js
@@ -2,14 +2,13 @@ import React from "react";
 
 export default function Question(props) {
 
-  console.log(props);
   return (
     <main className="question">
       <h2>Question</h2>
       <h4>{props.question.text}</h4>
-      <button>{props.responses[0].text}</button>
-      <button>{props.responses[1].text}</button>
-      <button>{props.responses[2].text}</button>
+      <button onClick={() => props.onResponse()}>{props.responses[0].text}</button>
+      <button onClick={() => props.onResponse()}>{props.responses[1].text}</button>
+      <button onClick={() => props.onResponse()}>{props.responses[2].text}</button>
     </main>
   )
 }
