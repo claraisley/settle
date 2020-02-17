cba6fc5d15d62585160b3dbb1911c74d4d99a3b8
Susan Zhang <susanwenzhang@gmail.com>
2020-02-17 11:09:35 -0500

    added usevisualmode function and created basic mood and question modes

--- client/src/components/Workthrough/Index.js
+++ client/src/components/Workthrough/Index.js
@@ -1,9 +1,22 @@
 import React from "react";
+import Mood from "./Mood.js";
+import Question from "./Question.js";
+import useVisualMode from "../../hooks/useVisualMode"
+
+const MOOD = "MOOD";
+const QUESTION = "QUESTION";
 
 export default function Workthrough() {
+
+  const { mode, transition, back } = useVisualMode(MOOD);
+
   return (
     <main className="workthrough">
       <h2>Workthrough</h2>
+      <section>
+        {mode === MOOD && <Mood />}
+        {mode === QUESTION && <Question />}
+      </section>
     </main>
   )
 }
