02d1e6f2e2acbd431700a319f3c6b94a77274028
Clara <isleyc97@gmail.com>
2020-02-17 18:04:14 -0500

    Restyled form

--- client/src/components/Signup/Form.js
+++ client/src/components/Signup/Form.js
@@ -1,22 +1,37 @@
-import React, {useState}  from 'react';
+import React from 'react';
 import useSignUpForm from "../../hooks/useSignUpForm";
 import Button from '@material-ui/core/Button';
+import CssBaseline from '@material-ui/core/CssBaseline';
 import TextField from '@material-ui/core/TextField';
-import { makeStyles } from '@material-ui/core/styles';
 import Grid from '@material-ui/core/Grid';
+import Box from '@material-ui/core/Box';
+import Typography from '@material-ui/core/Typography';
+import { makeStyles } from '@material-ui/core/styles';
+import Container from '@material-ui/core/Container';
 
 const axios = require("axios").default;
-  const useStyles = makeStyles(theme => ({
-    root: {
-      '& .MuiTextField-root': {
-        margin: theme.spacing(1),
-        width: 200,
-      },
-    },
-  }));
 
-export default function SignUpForm(props) {
+const useStyles = makeStyles(theme => ({
+  paper: {
+    marginTop: theme.spacing(8),
+    display: 'flex',
+    flexDirection: 'column',
+    alignItems: 'center',
+  },
+  avatar: {
+    margin: theme.spacing(1),
+    backgroundColor: theme.palette.secondary.main,
+  },
+  form: {
+    width: '100%', // Fix IE 11 issue.
+    marginTop: theme.spacing(3),
+  },
+  submit: {
+    margin: theme.spacing(3, 0, 2),
+  },
+}));
 
+export default function SignUp(props) {
   const classes = useStyles();
 
   const signUserUp = () => {
@@ -51,86 +66,100 @@ export default function SignUpForm(props) {
   const { inputs, handleInputChange, handleSubmit } = useSignUpForm(signUserUp);
 
   return (
-    <main className="signup">
-      <Grid
-  container
-  direction="column"
-  justify="flex-end"
-  alignItems="center"
->
-      <h2>Signup </h2>
-      <section>
-        <form className={classes.root} autoComplete="off" id="create-user-form" onSubmit={handleSubmit}>
-        <div>
-        <TextField
-          id="outlined-name"
-          name="firstName"
-          placeholder="Enter First Name"
-          type="text"
-          label="First Name"
-          value={inputs.firstName || ""}
-          onChange={handleInputChange}
-          variant="outlined"
-          required
-        />
-        </div>
-          <div>
-          <TextField
-          id="outlined-name"
-          name="lastName"
-          placeholder="Enter Last Name"
-          type="text"
-          label="Last Name"
-          value={inputs.lastName || ""}
-          onChange={handleInputChange}
-          variant="outlined"
-          required
-        />
-          </div>
-          <div>
-          <TextField
-          id="outlined-email"
-          name="email"
-          placeholder="Enter Email"
-          type="email"
-          label="email"
-          value={inputs.email || ""}
-          onChange={handleInputChange}
-          variant="outlined"
-          required
-        />
-          </div>
-          <div>
-          <TextField
-          id="outlined-password1"
-          name="password1"
-          placeholder="Enter Password"
-          type="password"
-          label="password"
-          value={inputs.password1 || ""}
-          onChange={handleInputChange}
-          variant="outlined"
-          required
-        />
-          </div>
-          <div>
-          <TextField
-          id="outlined-password2"
-          name="password2"
-          placeholder="Confirm Password"
-          type="password"
-          label="password"
-          value={inputs.password2 || ""}
-          onChange={handleInputChange}
-          variant="outlined"
-          required
-        />
-          </div>
-          <Button type="submit" variant="contained">Sign up</Button>
+    <Container component="main" maxWidth="xs">
+      <CssBaseline />
+      <div className={classes.paper}>
+        <Typography component="h1" variant="h5">
+          Sign Up
+        </Typography>
+        <form className={classes.form} autoComplete="off" onSubmit={handleSubmit}>
+          <Grid container spacing={2}>
+            <Grid item xs={12} sm={6}>
+              <TextField
+                name="firstName"
+                variant="outlined"
+                required
+                fullWidth
+                type="text"
+                id="firstName"
+                label="First Name"
+                value={inputs.firstName || ""}
+                onChange={handleInputChange}
+                autoFocus
+              />
+            </Grid>
+            <Grid item xs={12} sm={6}>
+              <TextField
+                variant="outlined"
+                required
+                fullWidth
+                id="lastName"
+                type="text"
+                label="Last Name"
+                name="lastName"
+                value={inputs.lastName || ""}
+                onChange={handleInputChange}
+              />
+            </Grid>
+            <Grid item xs={12}>
+              <TextField
+                variant="outlined"
+                required
+                fullWidth
+                id="email"
+                type="email"
+                label="Email Address"
+                name="email"
+                value={inputs.email || ""}
+                onChange={handleInputChange}
+              />
+            </Grid>
+            <Grid item xs={12}>
+              <TextField
+                variant="outlined"
+                required
+                fullWidth
+                name="password1"
+                label="Password"
+                type="password"
+                id="password1"
+                value={inputs.password1 || ""}
+                onChange={handleInputChange}
+              />
+            </Grid>
+            <Grid item xs={12}>
+              <TextField
+                variant="outlined"
+                required
+                fullWidth
+                name="password2"
+                label="Password"
+                type="password"
+                id="password2"
+                value={inputs.password2 || ""}
+                onChange={handleInputChange}
+              />
+            </Grid>
+          </Grid>
+          <Button
+            type="submit"
+            fullWidth
+            variant="contained"
+            color="primary"
+            className={classes.submit}
+          >
+            Sign Up
+          </Button>
+          <Grid container justify="flex-end">
+            <Grid item>
+            </Grid>
+          </Grid>
         </form>
-      </section>
-    </Grid>
-    </main>
+      </div>
+      <Box mt={5}>
+      </Box>
+    </Container>
   );
 }
+
 
