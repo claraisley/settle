653dadaa608f408b449d5e7a21d47e17c0733abf
Susan Zhang <susanwenzhang@gmail.com>
2020-02-17 14:55:00 -0500

    moved sample data to other file and rendering questions and responses from sample data

--- client/src/components/Workthrough/Index.js
+++ client/src/components/Workthrough/Index.js
@@ -1,10 +1,11 @@
-import React from "react";
+import React, { useState } from "react";
 import Mood from "./Mood.js";
 import Question from "./Question.js";
 import useVisualMode from "../../hooks/useVisualMode"
 import Followup from "./Followup.js";
 import Completion from "./Completion.js";
 import Start from "./Start.js";
+import sampleData from "./sampleData.js"
 
 const MOOD = "MOOD";
 const QUESTION = "QUESTION";
@@ -12,46 +13,35 @@ const FOLLOWUP = "FOLLOWUP";
 const COMPLETION = "COMPLETION";
 const START = "START"
 
-const sampleThoughtData = [
-  {
-    "id": 1,
-    "interest_id": "NIL",
-    "text": "If I mess this test up, will [pet name] be there for me when I get home?",
-  },
-  {
-    "id": 2,
-    "interest_id": "NIL",
-    "text": "People can tell when I am feeling anxious.",
-  },
-  {
-    "id": 3,
-    "interest_id": "NIL",
-    "text": "Text for example thought numero 3.",
-  },
-  {
-    "id": 4,
-    "interest_id": "NIL",
-    "text": "Text for example thought numero 4.",
-  },
-  {
-    "id": 5,
-    "interest_id": "NIL",
-    "text": "Text for example thought numero 5.",
-  },
-  {
-    "id": 6,
-    "interest_id": "NIL",
-    "text": "Text for example thought numero 6.",
-  }]
-
 export default function Workthrough() {
+
+  const [state, setState] = useState({
+    questions: [],
+    responses: []
+  })
 
   const { mode, transition, back } = useVisualMode(START);
 
   const startWorkthrough = (numberOfQuestions) => {
-    const shuffled = sampleThoughtData.sort(() => 0.5 - Math.random());
+    const shuffled = sampleData.sampleThoughtData.sort(() => 0.5 - Math.random());
     let selected = shuffled.slice(0, numberOfQuestions)
-    console.log(selected)
+    for (let question of selected) {
+      question.answered = false;
+    }
+    setState(prev => ({ ...prev, questions: selected, responses: sampleData.sampleResponseData }))
+    startNextQuestion();
+  }
+
+  const currentQuestion = state.questions.find(question => {
+    return question.answered === false;
+  })
+
+  const currentResponses = state.responses.filter(response => {
+    return response.thought === currentQuestion.id;
+  })
+
+  const startNextQuestion = () => {
+    transition(QUESTION)
   }
 
   return (
@@ -59,9 +49,9 @@ export default function Workthrough() {
       <h2>Workthrough</h2>
       <section>
         {mode === START && <Start
-        startWorkthrough={startWorkthrough} />}
+          startWorkthrough={startWorkthrough} />}
         {mode === MOOD && <Mood />}
-        {mode === QUESTION && <Question />}
+        {mode === QUESTION && <Question question={currentQuestion} responses={currentResponses} />}
         {mode === FOLLOWUP && <Followup />}
         {mode === COMPLETION && <Completion />}
       </section>
--- client/src/components/Workthrough/Question.js
+++ client/src/components/Workthrough/Question.js
@@ -1,13 +1,15 @@
 import React from "react";
 
-export default function Question() {
+export default function Question(props) {
+
+  console.log(props);
   return (
     <main className="question">
       <h2>Question</h2>
-      <h4>Are you worried your teacher will be disappointed?</h4>
-      <button>Yes</button>
-      <button>Maybe</button>
-      <button>No</button>
+      <h4>{props.question.text}</h4>
+      <button>{props.responses[0].text}</button>
+      <button>{props.responses[1].text}</button>
+      <button>{props.responses[2].text}</button>
     </main>
   )
 }
