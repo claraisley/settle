6c6a6d78d91fb92c296eb70bb79cb6ba9fda81f1
Susan Zhang <susanwenzhang@gmail.com>
2020-02-18 16:46:00 -0500

    added reflections controller that responds back with all thoughts and associated responses

--- app/controllers/reflections_controller.rb
+++ app/controllers/reflections_controller.rb
@@ -2,8 +2,7 @@ class ReflectionsController < ApplicationController
 
   def index
     @thoughts = Thought.all
-    
-    render json: @thoughts
+    render :json => @thoughts.to_json(:include => [:responses])
   end
 
 end
