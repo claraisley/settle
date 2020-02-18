f6928bca2f31a0be81879e77fd6df97ee9d7d921
Clara <isleyc97@gmail.com>
2020-02-18 17:05:08 -0500

    Added paper for questions

--- client/src/components/Workthrough/Question.js
+++ client/src/components/Workthrough/Question.js
@@ -1,15 +1,41 @@
 import React from "react";
 
+import { makeStyles } from '@material-ui/core/styles';
+import Paper from '@material-ui/core/Paper';
+
+const useStyles = makeStyles(theme => ({
+  root: {
+    display: 'flex',
+    flexWrap: 'wrap',
+    justifyContent: 'center',
+
+    '& > *': {
+      margin: theme.spacing(1),
+      // width: theme.spacing(16),
+      height: theme.spacing(64),
+      backgroundColor: '#353c52',
+    backgroundImage: `url(http://www.transparenttextures.com/patterns/cubes.png)`,
+    },
+  },
+}));
+
+
 export default function Question(props) {
+
+  const classes = useStyles();
 
   const responseList = props.responses.map(response => {
     return <button onClick={props.onResponse} key={response.id} >{response.text}</button> })
 
   return (
-    <main className="question">
+    <div className={classes.root}>
+      <Paper elevation={6}>
+   <main className="question">
       <h2>Question</h2>
       <h4>{props.question.text}</h4>
       {responseList}
     </main>
+      </Paper>
+    </div>
   )
 }
