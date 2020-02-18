4da58fa54fffa5385b8af8743fc39d6945925d70
Susan Zhang <susanwenzhang@gmail.com>
2020-02-18 11:48:05 -0500

    redirect from homepage added and got rid of old console.logs

--- client/src/components/App.js
+++ client/src/components/App.js
@@ -1,6 +1,6 @@
 import React from 'react';
 import './App.css';
-import { BrowserRouter as Router, Switch, Route, Redirect } from "react-router-dom";
+import { BrowserRouter as Router, Switch, Route, Redirect, useHistory } from "react-router-dom";
 import Nav from './Nav';
 import useApplicationData from "../hooks/useApplicationData.js";
 
@@ -10,34 +10,29 @@ function App() {
     state, links, authenticatetUser
   } = useApplicationData();
 
-
-  // getPages
-  // getUsers
-
   // const filteredLinks = links.filter(l => l.isProtected && state.user || l.isProtected === false)
   // const routes = filteredLinks.map((link, index) => {
-    const PrivateRoute = ({ component: Component, ...rest }) => (
-      <Route {...rest} render={(props) => (
-        authenticatetUser()
-          ? <Component {...props} />
-          : <Redirect to='/login' />
-      )} />
-    )
-    const routes = links.map((link, index) => {
-      //const component = link.requiresAuthentication && !authenticatetUser()? <Redirect to={{ pathname: '/login'}} /> : link.component
-      return link.requiresAuthentication 
+  const PrivateRoute = ({ component: Component, ...rest }) => (
+    <Route {...rest} render={(props) => (
+      authenticatetUser()
+        ? <Component {...props} />
+        : <Redirect to='/login' />
+    )} />
+  )
+  const routes = links.map((link, index) => {
+    //const component = link.requiresAuthentication && !authenticatetUser()? <Redirect to={{ pathname: '/login'}} /> : link.component
+    return link.requiresAuthentication
       ? <PrivateRoute
         key={index}
         path={link.path}
-        component={() => link.component} /> 
+        component={() => link.component} />
       : <Route
         key={index}
         path={link.path} >
         {link.component}
-        </Route>
-    
+      </Route>
   })
-  
+
   return (
     <div>
       <h1>settle</h1>
@@ -46,6 +41,7 @@ function App() {
         <Switch>
           {routes}
         </Switch>
+        <Redirect to='/signup'></Redirect>
       </Router>
     </div >
   );
