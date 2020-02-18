a41ce8143f89d5930aafe4afcdad02aaa44c3f74
Susan Zhang <susanwenzhang@gmail.com>
2020-02-18 11:35:21 -0500

    just fixing the emoji warnings

--- client/src/components/Workthrough/Mood.js
+++ client/src/components/Workthrough/Mood.js
@@ -5,9 +5,9 @@ export default function Mood(props) {
     <main className="Mood">
       <h2>Mood</h2>
       <h4>How are you feeling right now?</h4>
-      <button onClick={props.onResponse}>😟</button>
-      <button onClick={props.onResponse}>😐</button>
-      <button onClick={props.onResponse}>😃</button>
+      <button onClick={props.onResponse}><span role="img" aria-label="sad">😟</span></button>
+      <button onClick={props.onResponse}><span role="img" aria-label="neutral">😐</span></button>
+      <button onClick={props.onResponse}><span role="img" aria-label="happy">😃</span></button>
     </main>
   )
 }
