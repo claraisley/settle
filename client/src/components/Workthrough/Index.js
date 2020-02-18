d20b7f80ea5f45a6150e0dda12844f3cd26bf3d0
Susan Zhang <susanwenzhang@gmail.com>
2020-02-17 19:15:41 -0500

    It finally worksgit add .git add .git add .

--- client/src/components/Workthrough/Index.js
+++ client/src/components/Workthrough/Index.js
@@ -17,12 +17,30 @@ export default function Workthrough() {
 
   const [state, setState] = useState({
     questions: [],
-    responses: []
+    responses: [],
+    currentQuestion: {}
   })
 
-  console.log("STATE", state)
-
   const { mode, transition, back } = useVisualMode(START);
+
+  const startNextQuestion = () => {
+    transition(QUESTION)
+  }
+
+  const unansweredQuestions = state.questions.filter(question => !question.answered)
+  if (!state.currentQuestion.id && unansweredQuestions.length) {
+    const currentQuestion = unansweredQuestions.pop()
+    setState(prev => ({
+      ...prev,
+      currentQuestion
+    }))
+  } else if (!unansweredQuestions.length && state.questions.length && mode !== COMPLETION) {
+    transition(COMPLETION)
+  } 
+
+  const responses = state.responses.filter(response => {
+    return response.thought === state.currentQuestion.id;
+  })
 
   const startWorkthrough = (numberOfQuestions) => {
     const shuffled = sampleData.sampleThoughtData.sort(() => 0.5 - Math.random());
@@ -30,38 +48,23 @@ export default function Workthrough() {
     for (let question of selected) {
       question.answered = false;
     }
-    setState(prev => ({ ...prev, questions: selected, responses: sampleData.sampleResponseData }))
+    setState(prev => ({ ...prev, questions: selected, responses: sampleData.sampleResponseData }));
     startNextQuestion();
   }
 
-  const currentQuestion = state.questions.find(question => {
-    console.log("currentQuestion")
-    return question.answered === false;
-  })
-
-  const currentResponses = state.responses.filter(response => {
-    return response.thought === currentQuestion.id;
-  })
-
   const currentProgress = state.questions.filter(question => {
-    return question.answered === true
+    return question.answered === false
   }).length
 
   const respond = () => {
-    console.log("respond")
     setState(prev => ({
       ...prev,
       questions: [
-        ...state.questions.filter(({ id }) => id !== currentQuestion.id),
-        { ...currentQuestion, answered: true }
+        ...state.questions.filter(({ id }) => id !== state.currentQuestion.id),
+        { ...state.currentQuestion, answered: true }
       ],
+      currentQuestion: {}
     }))
-    startNextQuestion();
-  }
-
-  const startNextQuestion = () => {
-    console.log("start next question")
-    transition(QUESTION)
   }
 
   return (
@@ -71,14 +74,13 @@ export default function Workthrough() {
         {mode === START && <Start
           startWorkthrough={startWorkthrough} />}
         {mode === MOOD && <Mood />}
-        {mode === QUESTION && <Question question={currentQuestion} responses={currentResponses} onResponse={respond} />}
+        {mode === QUESTION && <Question question={state.currentQuestion} responses={responses} onResponse={respond} />}
         {mode === FOLLOWUP && <Followup />}
         {mode === COMPLETION && <Completion />}
       </section>
       <section>
-        <label htmlFor="workthrough-progress">Progress {currentProgress}/{state.questions.length}</label>
-        <progress id="workthrough-progress" value="{currentProgress}/{state.questions.length}" max="100" ></progress>
-        {/* <LinearProgress variant="determinate" value={completed} /> */}
+        <label htmlFor="workthrough-progress">Progress {state.questions.length - currentProgress}/{state.questions.length}</label>
+        <progress id="workthrough-progress" max="100" ></progress>
         <span role="img" aria-label="up-arrow">⬆</span>
         <span role="img" aria-label="down-arrow">⬇</span>
       </section>
--- client/src/components/Workthrough/Question.js
+++ client/src/components/Workthrough/Question.js
@@ -2,13 +2,14 @@ import React from "react";
 
 export default function Question(props) {
 
+  const responseList = props.responses.map(response => {
+    return <button onClick={props.onResponse}>{response.text}</button> })
+
   return (
     <main className="question">
       <h2>Question</h2>
       <h4>{props.question.text}</h4>
-      <button onClick={() => props.onResponse()}>{props.responses[0].text}</button>
-      <button onClick={() => props.onResponse()}>{props.responses[1].text}</button>
-      <button onClick={() => props.onResponse()}>{props.responses[2].text}</button>
+      {responseList}
     </main>
   )
 }
