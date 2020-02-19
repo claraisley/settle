2ea6460bb69c14593846f65f04957347c65baf62
Susan <susanwenzhang@gmail.com>
2020-02-19 16:48:23 -0500

    backend finally works for posting completed workthroughs :tada:

--- app/models/reflection.rb
+++ app/models/reflection.rb
@@ -2,4 +2,7 @@ class Reflection < ApplicationRecord
   has_many :moods
   has_many :reflection_responses
   has_many :responses, :through => :reflection_responses
+
+  accepts_nested_attributes_for :moods
+  accepts_nested_attributes_for :reflection_responses
 end
