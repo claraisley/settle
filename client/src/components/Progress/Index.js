33cf0eacecd77b08caa48fa76aaaa260b1c24e8b
Susan <susanwenzhang@gmail.com>
2020-02-19 22:23:25 -0500

    added calendar and starting working on it

--- client/src/components/Progress/Index.js
+++ client/src/components/Progress/Index.js
@@ -1,9 +1,15 @@
 import React from "react";
+import MoodCalendar from "./MoodCalendar";
+import ThinkingTrap from "./ThinkingTrap"
+import WorkthroughHistory from "./WorkthroughHistory"
 
 export default function Progress() {
   return (
     <main className="progress">
       <h2>Progress</h2>
+      <MoodCalendar />
+      <ThinkingTrap />
+      <WorkthroughHistory />
     </main>
   )
 }
---
+++ client/src/components/Progress/MoodCalendar.js
@@ -0,0 +1,15 @@
+import React from "react";
+import Calendar from 'react-calendar';
+
+export default function MoodCalendar() {
+  const tileContent = ({ date, view }) => view === 'month' && date.getDay() === 0 ? <p>Sunday!</p> : null;
+  const tileContent2 = ({date}) => `
+  ${date.getUTCMonth()} / ${date.getUTCDate()} / ${date.getUTCFullYear()}`;
+
+  return (
+    <main className="mood-calendar">
+      <h2>Mood Chart</h2>
+      <Calendar tileContent={tileContent2} />
+    </main>
+  )
+}
