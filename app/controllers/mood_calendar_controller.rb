f182b782579c7a281a76b275f5ca9cc4bf3ea360
Susan <susanwenzhang@gmail.com>
2020-02-20 12:28:38 -0500

    mood controller sends back user with nested reflections and moods

--- app/controllers/mood_calendar_controller.rb
+++ app/controllers/mood_calendar_controller.rb
@@ -1,6 +1,8 @@
 class MoodCalendarController < ApplicationController
 
   def index
+    @user = User.find(params[:user_id])
+    render :json => @user.to_json(:include => {:reflections => { :include => :moods }}), status: :ok
   end
-  
+
 end
