8145fb7527461604f6f6b182ecf570bd88802610
Susan <susanwenzhang@gmail.com>
2020-02-19 20:30:08 -0500

    material ui driving me nuts.... version of explore page with mui

--- client/src/components/ExploreCard.js
+++ client/src/components/ExploreCard.js
@@ -8,14 +8,24 @@ import CardMedia from '@material-ui/core/CardMedia';
 import Button from '@material-ui/core/Button';
 import Typography from '@material-ui/core/Typography';
 
-const useStyles = makeStyles({
+const useStyles = makeStyles(theme => ({
   root: {
     margin: 0,
+    color: theme.palette.text.secondary,
   },
   media: {
     // height: 140,
   },
-});
+  cardTitle: {
+    color: theme.palette.text.secondary,
+  },
+  body2: {
+    color: theme.palette.text.secondary,
+  },
+  subheading: {
+    color: theme.palette.text.secondary,
+  }
+}));
 
 export default function MediaCard() {
   const classes = useStyles();
@@ -25,14 +35,14 @@ export default function MediaCard() {
       <CardActionArea>
         <CardMedia
           className={classes.media}
-          image="/static/images/cards/contemplative-reptile.jpg"
+          image="https://via.placeholder.com/150"
           title="Contemplative Reptile"
         />
-        <CardContent>
-          <Typography gutterBottom variant="h5" component="h2">
+        <CardContent className={classes.content} >
+          <Typography gutterBottom variant="h5" component="h2" className={classes.cardTitle}>
             Lizard
           </Typography>
-          <Typography variant="body2" color="textSecondary" component="p">
+          <Typography color='grey[500]' variant={"caption"} className={"MuiTypography--subheading"} component="p" classes={{root: classes.cardText}}>
             Lizards are a widespread group of squamate reptiles, with over 6,000 species, ranging
             across all continents except Antarctica
           </Typography>
@@ -41,9 +51,6 @@ export default function MediaCard() {
       <CardActions>
         <Button size="small" color="primary">
           Share
-        </Button>
-        <Button size="small" color="primary">
-          Learn More
         </Button>
       </CardActions>
     </Card>
