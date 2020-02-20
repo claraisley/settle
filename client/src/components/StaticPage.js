8b62e10d602ea578768cf5913d8e9d6300c7a0a3
Clara <isleyc97@gmail.com>
2020-02-20 10:33:38 -0500

    BAck to master changes

--- client/src/components/StaticPage.js
+++ client/src/components/StaticPage.js
@@ -1,66 +1,10 @@
 import React from "react";
-import { makeStyles } from "@material-ui/core/styles";
-import ExpansionPanel from "@material-ui/core/ExpansionPanel";
-import ExpansionPanelDetails from "@material-ui/core/ExpansionPanelDetails";
-import ExpansionPanelSummary from "@material-ui/core/ExpansionPanelSummary";
-import Typography from "@material-ui/core/Typography";
-import ExpandMoreIcon from "@material-ui/icons/ExpandMore";
-import "./staticPage.css";
-
-const useStyles = makeStyles(theme => ({
-  root: {
-    width: "100%"
-  },
-  heading: {
-    fontSize: theme.typography.pxToRem(15),
-    flexBasis: "33.33%",
-    flexShrink: 0,
-    color: theme.palette.text.secondary
-  },
-  secondaryHeading: {
-    fontSize: theme.typography.pxToRem(15),
-    color: theme.palette.text.secondary
-  },
-  details: {
-    color: theme.palette.text.secondary
-  }
-}));
 
 export default function StaticPage(props) {
-  const classes = useStyles();
-  const [expanded, setExpanded] = React.useState(false);
-  const handleChange = panel => event => {
-    setExpanded(prevEx => (prevEx !== panel ? panel : false));
-  };
-
-  const items = props.data.map(data => {
-    return (
-      <ExpansionPanel
-        expanded={expanded === `panel${data.id}`}
-        key={data.id}
-        onChange={handleChange(`panel${data.id}`)}
-      >
-        <ExpansionPanelSummary
-          expandIcon={<ExpandMoreIcon />}
-          aria-controls={`panel${data.id}bh-content`}
-          id={`panel${data.id}bh-header`}
-        >
-          <Typography className={classes.heading}>{data.name}</Typography>
-        </ExpansionPanelSummary>
-        <ExpansionPanelDetails>
-          <Typography className={classes.details}>{data.text}</Typography>
-        </ExpansionPanelDetails>
-      </ExpansionPanel>
-    );
-  });
-
   return (
-    <body className={classes.root}>
-      <main>
-        <h2>{props.title}</h2>
-        <div></div>
-        {items}
-      </main>
-    </body>
+    <main className="static-page">
+      <h2>{props.title}</h2>
+      <p>{props.text}</p>
+    </main>
   );
 }
