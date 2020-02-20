d5aa3682034571917b2f0a6bcb6b683c9c49a800
Katherine Szelag <katherine.szelag@mail.utoronto.ca>
2020-02-20 00:03:21 -0500

    not working

--- app/controllers/reflections_controller.rb
+++ app/controllers/reflections_controller.rb
@@ -4,11 +4,30 @@ class ReflectionsController < ApplicationController
 
     #psuedo code for what I want to do:
     # send the user_id here with the get (from state)
+    # @user_id = (params[:user_id])
+    #@user_id = 2
+
+    @thoughts = Thought.where(interest_id: NIL)
+    # .or(Thought.where(interest_id:))
+    
+
+    # Post.where(id: 1).or(Post.where(title: 'Learn Rails'))
+
+    @interest_ids = UserInterest.select(:interest_id).where(["user_id = :user_id", {user_id: 2}]).to_a
+    puts "here"
+    puts @interest_ids
+
+    #YourModel.where("categories.id IN ? OR category_relationships.category_id IN ?", category_ids, category_ids)
+
+    
+
+    render :json => @interest_ids.to_json
+   
     # grab the interests associated with that user via the user_id
     # grab the recent questions(aka thoughts) they have done via reflections, reflection_responses
     # grab thoughts where interest is NULL && where interest lines up with what user
-    @thoughts = Thought.limit(params[:number])
-    render :json => @thoughts.to_json(:include => {:responses => { :include => [:follow_ups, :thinking_trap] }})
+    # @thoughts = Thought.limit(params[:number])
+    # render :json => @thoughts.to_json(:include => {:responses => { :include => [:follow_ups, :thinking_trap] }})
   end
 
   def create
