4d224f87b5e84913e1bcdd4ba4fc1ef4daecd26f
Susan <susanwenzhang@gmail.com>
2020-02-21 14:56:00 -0500

    bar chart pulls info and component checks if user has any meditations

--- client/src/components/Progress/MeditationHistory.js
+++ client/src/components/Progress/MeditationHistory.js
@@ -7,57 +7,6 @@ export default function MeditationHistory(props) {
     meditations: [],
     baseDay: new Date()
   })
-  // console.log("meditations in state", state.meditations)
-
-  // gets Sunday as the start of week for the graph
-  let dayOfWeek = state.baseDay.getDay()
-  let startOfWeek = new Date();
-  startOfWeek.setDate(state.baseDay.getDate() - dayOfWeek)
-
-  // makes an array for the week, key is the actual date object, value is a string to put in the graph
-  let weekObject = {}
-  // weekObject[startOfWeek.toDateString()] = {displayDate: startOfWeek.toDateString().substr(4, 6), time: 0};
-  for (let i = 0; i < 7; i++) {
-    let weekday = new Date()
-    weekday.setDate(startOfWeek.getDate() + i)
-    weekObject[weekday.toDateString()] = {
-      displayDate: weekday.toDateString().substr(4, 6),
-      time: 0
-    }
-  }
-  console.log("weekobject", weekObject)
-
-  // builds an array objects of user's meditations: key is the date and value is the time
-  let meditationArray = [];
-  for (let meditation of state.meditations) {
-    meditationArray.push({ [meditation.created_at]: meditation.meditation.time_in_minutes })
-  }
-
-  console.log("meditation array for user", meditationArray)
-  // needs to result in an array that's just the minutes for each day in weekobject
-  for (let meditation of meditationArray) {
-    let formatMeditationDay = new Date(Object.keys(meditation)[0]).toDateString()
-    if (formatMeditationDay in weekObject) {
-      weekObject[formatMeditationDay].time += Object.values(meditation)[0]
-    }
-  }
-
-  const example = {
-    options: {
-      chart: {
-        id: "basic-bar"
-      },
-      xaxis: {
-        categories: Object.values(weekObject).map(({ displayDate }) => displayDate)
-      }
-    },
-    series: [
-      {
-        name: "series-1",
-        data: [30, 40, 45, 50, 49, 60, 70]
-      }
-    ]
-  };
 
   useEffect(() => {
     axios.request({
@@ -82,16 +31,66 @@ export default function MeditationHistory(props) {
       });
   }, [props.user.id]);
 
+  // gets Sunday as the start of week for the graph
+  let dayOfWeek = state.baseDay.getDay()
+  let startOfWeek = new Date();
+  startOfWeek.setDate(state.baseDay.getDate() - dayOfWeek)
+
+  // makes an array for the week, key is the actual date object, value is a string to put in the graph
+  let weekObject = {}
+  for (let i = 0; i < 7; i++) {
+    let weekday = new Date()
+    weekday.setDate(startOfWeek.getDate() + i)
+    weekObject[weekday.toDateString()] = {
+      displayDate: weekday.toDateString().substr(4, 6),
+      time: 0
+    }
+  }
+  // console.log("weekobject", weekObject)
+
+  // builds an array objects of user's meditations: key is the date and value is the time
+  let meditationArray = [];
+  for (let meditation of state.meditations) {
+    meditationArray.push({ [meditation.created_at]: meditation.meditation.time_in_minutes })
+  }
+
+  // console.log("meditation array for user", meditationArray)
+  // needs to result in an array that's just the minutes for each day in weekobject
+  for (let meditation of meditationArray) {
+    let formatMeditationDay = new Date(Object.keys(meditation)[0]).toDateString()
+    if (formatMeditationDay in weekObject) {
+      weekObject[formatMeditationDay].time += Object.values(meditation)[0]
+    }
+  }
+
+  const chartData = {
+    options: {
+      chart: {
+        id: "basic-bar"
+      },
+      xaxis: {
+        categories: Object.values(weekObject).map(({ displayDate }) => displayDate)
+      }
+    },
+    series: [
+      {
+        name: "series-1",
+        data: Object.values(weekObject).map(({ time }) => time)
+      }
+    ]
+  };
 
   return (
     <main className="MeditationHistory">
       <h2>My Previous Meditations</h2>
       <p>Your meditations for the week of {startOfWeek.toDateString()}</p>
-      <Chart
-        type="bar"
-        options={example.options}
-        series={example.series}
-        width="500" />
+      {state.meditations.length > 0 ?
+        <Chart
+          type="bar"
+          options={chartData.options}
+          series={chartData.series}
+          width="500" /> : <p>Do a meditation to start tracking your progress!</p>
+      }
     </main>
   )
 }
