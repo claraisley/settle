0fa8748e47b42f9fe81088c03f7dfdebc84ffa62
Katherine Szelag <katherine.szelag@mail.utoronto.ca>
2020-02-21 14:51:41 -0500

    thoughts now sorted by which the user has not recently seen, including one interest based one always. backend data for thinking traps is set up

--- app/models/thought.rb
+++ app/models/thought.rb
@@ -1,4 +1,18 @@
 class Thought < ApplicationRecord
   belongs_to :interest, optional: true
   has_many :responses
+
+  def most_recent_for(user)
+    resp = user.reflection_responses
+      .map(&:response)
+      .filter {|resp| resp.thought == self}
+      .sort { |a,b| b.created_at <=> a.created_at }
+      .first
+
+    if resp.nil?
+      0
+    else
+      resp.created_at.to_i
+    end
+  end
 end
