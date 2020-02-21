9fa1c0959a0c9ce606222dcacf3072c87230739d
Susan <susanwenzhang@gmail.com>
2020-02-20 20:54:48 -0500

    bar chart renders with example data

--- client/src/components/Progress/MeditationHistory.js
+++ client/src/components/Progress/MeditationHistory.js
@@ -9,32 +9,50 @@ export default function MeditationHistory(props) {
 
   console.log("MEDITATIONS IN STATE", state.meditations)
 
-  useEffect(() => {
-    axios.request({
-      url: "http://localhost:3001/meditations",
-      method: "get",
-      headers: {
-        "Content-Type": "application/json",
-        Accept: "application/json",
-        "Access-Control-Allow-Credentials": true
+  const example = {
+    options: {
+      chart: {
+        id: "basic-bar"
       },
-      params: {
-        user_id: props.user.id
-      },
-      withCredentials: true
+      xaxis: {
+        categories: [1991, 1992, 1993, 1994, 1995, 1996, 1997, 1998, 1999]
+      }
+    },
+    series: [
+      {
+        name: "series-1",
+        data: [30, 40, 45, 50, 49, 60, 70, 91]
+      }
+    ]
+  };
+
+useEffect(() => {
+  axios.request({
+    url: "http://localhost:3001/meditations",
+    method: "get",
+    headers: {
+      "Content-Type": "application/json",
+      Accept: "application/json",
+      "Access-Control-Allow-Credentials": true
+    },
+    params: {
+      user_id: props.user.id
+    },
+    withCredentials: true
+  })
+    .then(response => {
+      setState(prev => ({ ...prev, meditations: response.data })) // if no meditations, then state.meditations is just an empty array
     })
-      .then(response => {
-        setState(prev => ({ ...prev, meditations: response.data })) // if no meditations, then state.meditations is just an empty array
-      })
-      .catch(function (error) {
-        console.log(error);
-      });
-  }, []);
+    .catch(function (error) {
+      console.log(error);
+    });
+}, []);
 
 
-  return (
-    <main className="MeditationHistory">
-      <h2>My Previous Meditations</h2>
-    </main>
-  )
+return (
+  <main className="MeditationHistory">
+    <h2>My Previous Meditations</h2>
+    <Chart type="bar" options={example.options} series={example.series} width="500" />
+  </main>
+)
 }
