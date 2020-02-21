cbf654103bd247a22c588f7dac9a4cd5adeaeb41
Susan <susanwenzhang@gmail.com>
2020-02-21 12:33:53 -0500

    x-axis with dates is working

--- client/src/components/Progress/MeditationHistory.js
+++ client/src/components/Progress/MeditationHistory.js
@@ -4,10 +4,22 @@ import Chart from "react-apexcharts";
 
 export default function MeditationHistory(props) {
   const [state, setState] = useState({
-    meditations: []
+    meditations: [],
+    baseDay: new Date()
   })
 
-  console.log("MEDITATIONS IN STATE", state.meditations)
+  // getting start of week for the graph...yes this is weird
+  let dayOfWeek = state.baseDay.getDay()
+  let startOfWeek = new Date(state.baseDay.setDate(state.baseDay.getDate() - dayOfWeek))
+
+  // makes an array for the week, key is the actual date object, value is a string to put in the graph
+  let weekObject = {}
+  weekObject[startOfWeek] = startOfWeek.toDateString().substr(4, 6)
+  for (let i = 0; i < 6; i++) {
+    let weekday = new Date(startOfWeek.setDate(startOfWeek.getDate() + 1))
+    weekObject[weekday] = weekday.toDateString().substr(4, 6)
+  }
+  console.log("objects values", Object.values(weekObject))
 
   const example = {
     options: {
@@ -15,44 +27,49 @@ export default function MeditationHistory(props) {
         id: "basic-bar"
       },
       xaxis: {
-        categories: [1991, 1992, 1993, 1994, 1995, 1996, 1997, 1998, 1999]
+        categories: Object.values(weekObject)
       }
     },
     series: [
       {
         name: "series-1",
-        data: [30, 40, 45, 50, 49, 60, 70, 91]
+        data: [30, 40, 45, 50, 49, 60, 70]
       }
     ]
   };
 
-useEffect(() => {
-  axios.request({
-    url: "http://localhost:3001/meditations",
-    method: "get",
-    headers: {
-      "Content-Type": "application/json",
-      Accept: "application/json",
-      "Access-Control-Allow-Credentials": true
-    },
-    params: {
-      user_id: props.user.id
-    },
-    withCredentials: true
-  })
-    .then(response => {
-      setState(prev => ({ ...prev, meditations: response.data })) // if no meditations, then state.meditations is just an empty array
+  useEffect(() => {
+    axios.request({
+      url: "http://localhost:3001/meditations",
+      method: "get",
+      headers: {
+        "Content-Type": "application/json",
+        Accept: "application/json",
+        "Access-Control-Allow-Credentials": true
+      },
+      params: {
+        user_id: props.user.id
+      },
+      withCredentials: true
     })
-    .catch(function (error) {
-      console.log(error);
-    });
-}, []);
+      .then(response => {
+        setState(prev => ({ ...prev, meditations: response.data })) // if no meditations, then state.meditations is just an empty array
+      })
+      .catch(function (error) {
+        console.log(error);
+      });
+  }, [props.user.id]);
 
 
-return (
-  <main className="MeditationHistory">
-    <h2>My Previous Meditations</h2>
-    <Chart type="bar" options={example.options} series={example.series} width="500" />
-  </main>
-)
+  return (
+    <main className="MeditationHistory">
+      <h2>My Previous Meditations</h2>
+      <p>Your meditations for the week of {startOfWeek.toDateString()}</p>
+      <Chart
+        type="bar"
+        options={example.options}
+        series={example.series}
+        width="500" />
+    </main>
+  )
 }
