5ff9eaf968b7424e8f19557db27c36a1478679b5
Susan <susanwenzhang@gmail.com>
2020-02-22 18:34:07 -0500

    added a tooltip to the followup for the related thinking traps

--- client/src/components/Workthrough/Followup.js
+++ client/src/components/Workthrough/Followup.js
@@ -1,13 +1,41 @@
 import React from "react";
 import personalizeText from "./HelperFunction"
+import Tooltip from '@material-ui/core/Tooltip';
+import Zoom from '@material-ui/core/Zoom';
+import styled from "styled-components";
+import { makeStyles } from '@material-ui/core/styles';
+
+// styling for later: https://stackoverflow.com/questions/36759985/how-to-style-material-uis-tooltip/54606987#54606987
+
+const useStyles = makeStyles(theme => ({
+  customWidth: {
+    maxWidth: 500,
+  }
+}));
+
+const TooltipText = styled.span`
+  font-style: italic;
+  font-weight: 600;
+  font-size: 1rem;
+`;
+
+const Tooltiptip = styled.span`
+  font-size: 1rem;
+  line-height: 150%;
+  display: inline-block;
+  margin: 10px;
+`;
 
 export default function Followup(props) {
+  const classes = useStyles();
+
 
   return (
     <main className="followup">
       <p>{personalizeText(props.interests, props.followup[0].text)}</p>
-      <h4>Related thinking trap: {props.thinkingTrap.name}</h4>
-      <p>{props.thinkingTrap.definition}</p>
+      <Tooltip TransitionComponent={Zoom} title={<Tooltiptip>{props.thinkingTrap.definition}</Tooltiptip>} classes={{ tooltip: classes.customWidth }} arrow>
+        <TooltipText>Related thinking trap: {props.thinkingTrap.name}<sup>[?]</sup></TooltipText>
+      </Tooltip>
     </main>
   )
 }
