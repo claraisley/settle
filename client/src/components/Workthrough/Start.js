1dbfa93ed15aa748b483a6d9a12013913fd691ea
Katherine Szelag <katherine.szelag@mail.utoronto.ca>
2020-02-25 15:22:13 -0500

    blurb on work-throughs

--- client/src/components/Workthrough/Start.js
+++ client/src/components/Workthrough/Start.js
@@ -37,7 +37,27 @@ const CardActionStart = styled(CardActions)`
 //     text-align: center;
 //   }
 // `;
+const StaticPaper2 = styled(Paper)`
+  width: 90%;
+  margin: 10px auto;
+  background-color: #353c52;
+  justify-content: center;
+  align-items: center;
+  padding: 1%;
+`;
 
+const Title = styled.h1`
+  text-align: center;
+  margin: 20px auto;
+  marfin-top: 5%;
+`;
+
+const Text = styled.p`
+  line-height: 1.5;
+  font-size: 1.5em;
+  font-weight: normal;
+  margin: 15px;
+`;
 const TypographyStart = styled(Typography)`
   font-size: x-large;
   font-weight: 600;
@@ -75,6 +95,17 @@ export default function Start(props) {
 
   return (
     <main className="start">
+      <Title>Work-Throughs</Title>
+      <StaticPaper2 elevation={10}>
+        <Text>
+          Thoughts are the things that we say to ourselves without speaking out
+          loud (self-talk). Each of us have our own way of thinking about
+          things, but it's important to recognize that our self-talk has a big
+          effect on how we feel. As a test approaches and you feel anxious, use
+          these Work-Throughs to identify which Thinking Traps you are falling
+          into, and to help you challenge anxious thoughts as they arise.{" "}
+        </Text>
+      </StaticPaper2>
       <div className={classes.root}>
         <Paper elevation={10}>
           <CardStart className={classes.cards}>
