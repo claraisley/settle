53ca4601a5c7ad6f30ab1a23d24bcdebf8b11fd7
Susan <susanwenzhang@gmail.com>
2020-02-27 12:57:07 -0500

    menu page is resized

--- client/src/components/Menu.js
+++ client/src/components/Menu.js
@@ -3,26 +3,30 @@ import Card from "@material-ui/core/Card";
 import CardActionArea from "@material-ui/core/CardActionArea";
 import CardContent from "@material-ui/core/CardContent";
 import Grid from "@material-ui/core/Grid";
-import Paper from "@material-ui/core/Paper";
 import styled from "styled-components";
 import CardHeader from "@material-ui/core/CardHeader";
-import CardMedia from "@material-ui/core/CardMedia";
 import { useHistory } from "react-router-dom";
 
 const StyledCard = styled(Card)`
   background-color: #353c52;
-  margin-top: 5%;
-  padding-top: 12px;
-  padding-bottom: 12px;
-`;
-const StyledPaper = styled(Paper)`
-  background-color: #353c52;
-  margin: 5%;
-  margin-top: 5%;
-  padding: 5%;
+  padding-top: 0.5rem;
+  padding-bottom: 0.5rem;
+  width: 100%;
+  height: 36vh;
 `;
 const StyledCardHeader = styled(CardHeader)`
   color: #ffd882;
+`;
+const StyledImg = styled.img`
+  width: 22vh;
+`;
+const StyledCardActionArea = styled(CardActionArea)`
+  display: flex;
+  justify-content: center;
+  flex-direction: column;
+`;
+const StyledMain = styled.main`
+  margin-top: 2rem;
 `;
 
 export default function Menu() {
@@ -30,90 +34,83 @@ export default function Menu() {
   const redirect = path => {
     history.push(path);
   };
+
   return (
-    <main>
-      <StyledPaper elevation={12}>
-        <Grid container alignItems="center" spacing={3}>
-          <Grid item xs={12} sm={4}>
-            <StyledCard >
-              <CardActionArea onClick={() => redirect("/progress")}>
-                <StyledCardHeader title="My Progress" />
-                <CardContent>
-                  <CardMedia
-                    component="img"
-                    image="https://res.cloudinary.com/dpfixnpii/image/upload/v1582598169/graphic_1_v82tbt.svg"
-                  />
-                </CardContent>
-              </CardActionArea>
-            </StyledCard>
-          </Grid>
-          <Grid item xs={12} sm={4}>
-            <StyledCard >
-              <CardActionArea onClick={() => redirect("/meditations")}>
-                <StyledCardHeader title="Meditations" />
-                <CardContent>
-                  <CardMedia
-                    component="img"
-                    image="https://res.cloudinary.com/dpfixnpii/image/upload/v1582598360/yoga_frq3hc.svg"
-                  />
-                </CardContent>
-              </CardActionArea>
-            </StyledCard>
-          </Grid>
-          <Grid item xs={12} sm={4}>
-            <StyledCard >
-              <CardActionArea onClick={() => redirect("/workthrough")}>
-                <StyledCardHeader title="Work-Throughs" />
-                <CardContent>
-                  <CardMedia
-                    component="img"
-                    image="https://res.cloudinary.com/dpfixnpii/image/upload/v1582598514/question_wk8bwh.svg   "
-                  />
-                </CardContent>
-              </CardActionArea>
-            </StyledCard>
-          </Grid>
-          <Grid item xs={12} sm={4}>
-            <StyledCard >
-              <CardActionArea onClick={() => redirect("/test-anxiety")}>
-                <StyledCardHeader title="What is Test Anxiety?" />
-                <CardContent>
-                  <CardMedia
-                    component="img"
-                    image="https://res.cloudinary.com/dpfixnpii/image/upload/v1582599106/exam_hdwxnf.svg"
-                  />
-                </CardContent>
-              </CardActionArea>
-            </StyledCard>
-          </Grid>
-          <Grid item xs={12} sm={4}>
-            <StyledCard >
-              <CardActionArea onClick={() => redirect("/thinking-traps")}>
-                <StyledCardHeader title="Thinking Traps" />
-                <CardContent>
-                  <CardMedia
-                    component="img"
-                    image="https://res.cloudinary.com/dpfixnpii/image/upload/v1582599362/fishing-net_ld2ofp.svg"
-                  />
-                </CardContent>
-              </CardActionArea>
-            </StyledCard>
-          </Grid>
-          <Grid item xs={12} sm={4}>
-            <StyledCard >
-              <CardActionArea onClick={() => redirect("/tips-tricks")}>
-                <StyledCardHeader title="Dos and Don'ts" />
-                <CardContent>
-                  <CardMedia
-                    component="img"
-                    image="https://res.cloudinary.com/dpfixnpii/image/upload/v1582599443/right_mhzxwa.svg"
-                  />
-                </CardContent>
-              </CardActionArea>
-            </StyledCard>
-          </Grid>
+    <StyledMain>
+      <Grid container alignItems="center" spacing={3}>
+        <Grid item xs={12} sm={4}>
+          <StyledCard >
+            <StyledCardActionArea onClick={() => redirect("/progress")}>
+              <StyledCardHeader title="My Progress" />
+              <CardContent>
+                <StyledImg
+                  src="https://res.cloudinary.com/dpfixnpii/image/upload/v1582598169/graphic_1_v82tbt.svg"
+                />
+              </CardContent>
+            </StyledCardActionArea>
+          </StyledCard>
         </Grid>
-      </StyledPaper>
-    </main>
+        <Grid item xs={12} sm={4}>
+          <StyledCard >
+            <StyledCardActionArea onClick={() => redirect("/meditations")}>
+              <StyledCardHeader title="Meditations" />
+              <CardContent>
+                <StyledImg
+                  src="https://res.cloudinary.com/dpfixnpii/image/upload/v1582598360/yoga_frq3hc.svg"
+                />
+              </CardContent>
+            </StyledCardActionArea>
+          </StyledCard>
+        </Grid>
+        <Grid item xs={12} sm={4}>
+          <StyledCard >
+            <StyledCardActionArea onClick={() => redirect("/workthrough")}>
+              <StyledCardHeader title="Work-Throughs" />
+              <CardContent>
+                <StyledImg
+                  src="https://res.cloudinary.com/dpfixnpii/image/upload/v1582598514/question_wk8bwh.svg   "
+                />
+              </CardContent>
+            </StyledCardActionArea>
+          </StyledCard>
+        </Grid>
+        <Grid item xs={12} sm={4}>
+          <StyledCard >
+            <StyledCardActionArea onClick={() => redirect("/test-anxiety")}>
+              <StyledCardHeader title="What is Test Anxiety?" />
+              <CardContent>
+                <StyledImg
+                  src="https://res.cloudinary.com/dpfixnpii/image/upload/v1582599106/exam_hdwxnf.svg"
+                />
+              </CardContent>
+            </StyledCardActionArea>
+          </StyledCard>
+        </Grid>
+        <Grid item xs={12} sm={4}>
+          <StyledCard >
+            <StyledCardActionArea onClick={() => redirect("/thinking-traps")}>
+              <StyledCardHeader title="Thinking Traps" />
+              <CardContent>
+                <StyledImg
+                  src="https://res.cloudinary.com/dpfixnpii/image/upload/v1582599362/fishing-net_ld2ofp.svg"
+                />
+              </CardContent>
+            </StyledCardActionArea>
+          </StyledCard>
+        </Grid>
+        <Grid item xs={12} sm={4}>
+          <StyledCard >
+            <StyledCardActionArea onClick={() => redirect("/tips-tricks")}>
+              <StyledCardHeader title="Dos and Don'ts" />
+              <CardContent>
+                <StyledImg
+                  src="https://res.cloudinary.com/dpfixnpii/image/upload/v1582599443/right_mhzxwa.svg"
+                />
+              </CardContent>
+            </StyledCardActionArea>
+          </StyledCard>
+        </Grid>
+      </Grid>
+    </StyledMain>
   );
 }
